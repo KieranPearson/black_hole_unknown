656f94c1f8413ac3754cb7a91c3541c8a738306f
Kieran <61248872+KieranPearson@users.noreply.github.com>
2022-03-21 20:28:55 +0000

    Added SpriteRenderer requirement to Movement component

--- Assets/Scripts/Movement/Movement.cs
+++ Assets/Scripts/Movement/Movement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float speed;
