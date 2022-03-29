a88c1592059e18d357e8ff4e6aa74f8e8cbaa040
Kieran <61248872+KieranPearson@users.noreply.github.com>
2022-03-29 19:01:46 +0100

    Added menu transitions

--- Assets/Scripts/UI Scripts/MainMenuHandler.cs
+++ Assets/Scripts/UI Scripts/MainMenuHandler.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class MainMenuHandler : MonoBehaviour
 {
+    private UIController uiController;
+
+    private void Awake()
+    {
+        uiController = transform.parent.gameObject.GetComponent<UIController>();
+    }
+
     public void PlayButtonClicked()
     {
-
+        uiController.DisplayProfileSelector();
     }
 
     public void AchievementsButtonClicked()
     {
-
+        uiController.DisplayAchievements();
     }
 
     public void SettingsButtonClicked()
     {
-
+        uiController.DispalySettings();
     }
 
     public void CreditsButtonClicked()
     {
-
+        uiController?.DisplayCredits();
     }
 
     public void ExitButtonClicked()
--- Assets/Scripts/UI Scripts/UIController.cs
+++ Assets/Scripts/UI Scripts/UIController.cs
@@ -9,6 +9,7 @@ public class UIController : MonoBehaviour
     [SerializeField] private GameObject achievements;
     [SerializeField] private GameObject settings;
     [SerializeField] private GameObject credits;
+    [SerializeField] private float transitionSpeed;
 
     private CanvasGroup mainMenuGroup;
     private CanvasGroup profileSelectorGroup;
@@ -25,6 +26,7 @@ public class UIController : MonoBehaviour
 
     private CanvasGroup activeMenuGroup;
     private Transition activeTransition;
+    private CanvasGroup transitionTo;
 
     void Awake()
     {
@@ -39,30 +41,104 @@ public class UIController : MonoBehaviour
     {
         activeMenuGroup = mainMenuGroup;
         activeTransition = Transition.None;
+        transitionTo = null;
     }
 
-    private void TransitionOut(CanvasGroup canvasGroup)
+    private void DisplayMenu(CanvasGroup canvasGroup)
     {
-        //mainMenuCanvasGroup.alpha = 0;
+        if (activeMenuGroup == canvasGroup) return;
+        if (activeTransition == Transition.None)
+        {
+            transitionTo = canvasGroup;
+            activeTransition = Transition.Out;
+        }
     }
 
     public void DisplayProfileSelector()
     {
-        //TransitionOutMainMenu();
+        DisplayMenu(profileSelectorGroup);
     }
 
     public void DisplayAchievements()
     {
-
+        DisplayMenu(achievementsGroup);
     }
 
     public void DispalySettings()
     {
-
+        DisplayMenu(settingsGroup);
     }
 
     public void DisplayCredits()
     {
+        DisplayMenu(creditsGroup);
+    }
 
+    private void Update()
+    {
+        TransitionMenu();
+    }
+
+    private void TransitionMenu()
+    {
+        if (activeTransition == Transition.Out)
+        {
+            TransitionOutMenu();
+        } else if (activeTransition == Transition.In)
+        {
+            TransitionInMenu();
+        }
+    }
+
+    private void TransitionOutMenu()
+    {
+        if (activeMenuGroup.alpha > 0)
+        {
+            activeMenuGroup.alpha -= (Time.deltaTime * transitionSpeed);
+        } else
+        {
+            GetMenuGroupGameObject(activeMenuGroup).SetActive(false);
+            transitionTo.alpha = 0;
+            GetMenuGroupGameObject(transitionTo).SetActive(true);
+            activeTransition = Transition.In;
+        }
+    }
+
+    private void TransitionInMenu()
+    {
+        if (transitionTo.alpha < 1)
+        {
+            transitionTo.alpha += (Time.deltaTime * transitionSpeed);
+        } else
+        {
+            activeMenuGroup = transitionTo;
+            transitionTo = null;
+            activeTransition = Transition.None;
+        }
+    }
+
+    private GameObject GetMenuGroupGameObject(CanvasGroup canvasGroup)
+    {
+        if (canvasGroup == mainMenuGroup)
+        {
+            return mainMenu;
+        }
+        else if (canvasGroup == profileSelectorGroup)
+        {
+            return profileSelector;
+        }
+        else if (canvasGroup == achievementsGroup)
+        {
+            return achievements;
+        }
+        else if (canvasGroup == settingsGroup)
+        {
+            return settings;
+        }
+        else if (canvasGroup == creditsGroup)
+        {
+            return credits;
+        }
+        return null;
     }
 }
