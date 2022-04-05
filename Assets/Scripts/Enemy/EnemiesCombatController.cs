b13201ecf95cd26353dced790749ebb95226a1fb
Kieran <61248872+KieranPearson@users.noreply.github.com>
2022-04-05 18:28:00 +0100

    Adjusted enemy fire chance value

--- Assets/Scripts/Enemy/EnemiesCombatController.cs
+++ Assets/Scripts/Enemy/EnemiesCombatController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemiesCombatController : MonoBehaviour
 {
+    [SerializeField] private int chanceEnemyFires;
+
     public static event System.Action<GameObject> OnEnemyShoot;
 
     void FixedUpdate()
@@ -16,7 +18,7 @@ public class EnemiesCombatController : MonoBehaviour
         List<List<GameObject>> aliveEnemies = LevelManager.GetAliveEnemies();
         if (aliveEnemies.Count == 0) return;
 
-        int chanceEnemyShoots = Random.Range(0, 15);
+        int chanceEnemyShoots = Random.Range(0, chanceEnemyFires);
         if (chanceEnemyShoots != 0) return;
         int columnSelected = Random.Range(0, aliveEnemies.Count);
         List<GameObject> column = aliveEnemies[columnSelected];
